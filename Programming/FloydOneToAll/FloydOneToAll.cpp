2cc2f8809cfc116d4e95e2cbccdb08c5058d446e
erkkiperkele <aymeric.grail@gmail.com>
2014-11-15 13:28:24 -0500

    Read matrix frmo file

--- Programming/FloydOneToAll/FloydOneToAll.cpp
+++ Programming/FloydOneToAll/FloydOneToAll.cpp
@@ -9,71 +9,63 @@
 
 using namespace std;
 
+vector<int> LoadFromFile();
 void PrintChrono(int &nodes, size_t qty, double &duration);
 void CreateGraph();
 
 int _nodes;
-valarray<int> _graph;
 
 
 int main(int argc, char* argv[])
 {
-    int mpiRank;
-    int mpiSize;
-    
+	int mpiRank;
+	int mpiSize;
+
 	//initialize MPI, global variables and start chrono
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &mpiRank);
 	MPI_Comm_size(MPI_COMM_WORLD, &mpiSize);
 	double startTime = MPI_Wtime();
-	
-    CreateGraph();
+
+	vector<int> _graph = LoadFromFile();
 
 	if (mpiRank == 0)
-	{	
+	{
 		//Stop chrono and print results
 		double endTime = MPI_Wtime();
 		double duration = endTime - startTime;
-
+		cout << "test3" << endl;
 		cout << "Duration: " << duration << " seconds" << endl;
-//		PrintChrono(mpiSize, graph.Count(), duration);
+		//		PrintChrono(mpiSize, graph.Count(), duration);
 	}
 
 	MPI_Finalize();
 	return 0;
 }
 
-void CreateGraph()
-{
-    int _nodes = 36;
-    
-    int graphFlat[] =
-    {
-        0,1,3,0,0,3,
-        1,0,5,1,0,0,
-        3,5,0,2,1,0,
-        0,1,2,0,4,0,
-        0,0,1,4,0,5,
-        3,0,0,0,5,0
-    };
-    
-    valarray<int> graph(graphFlat, _nodes);
-    _graph = graph;
-    
-//    int i = 0;
-//    while (i < _nodes)
-//    {
-//        cout << "array: " << graphFlat[i] << endl;
-//        cout << "valarray: " << graph[i] << endl;
-//        ++i;
-//    }
-
-}
-
 void PrintChrono(int &nodes, size_t qty, double &duration)
 {
-    ofstream myfile;
+	ofstream myfile;
 	myfile.open("./FloydOneToAllChrono.csv", ios::app);
 	myfile << nodes << "\t" << qty << "\t" << duration << "\n";
 	myfile.close();
+}
+
+vector<int> LoadFromFile()
+{
+	ifstream input("./input.txt", ios::in);
+
+	int number;
+	vector<int> numbers;
+
+	if (input.is_open())
+	{
+		while (input >> number)
+		{
+			numbers.push_back(number);
+			input.get();
+		}
+		input.close();
+	}
+	return numbers;
 }
