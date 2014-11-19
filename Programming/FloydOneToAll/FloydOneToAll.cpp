f20ae94d34f2d389e038808f2876d4f823611e02
erkkiperkele <aymeric.grail@gmail.com>
2014-11-19 00:55:46 -0500

    Not tested. Plenty of errors but I think the main idea is there for the submatrices calculations. Still need to gather the matrices together.

--- Programming/FloydOneToAll/FloydOneToAll.cpp
+++ Programming/FloydOneToAll/FloydOneToAll.cpp
@@ -24,10 +24,11 @@ void CreateGraph();
 void MpiGroupInit();
 
 
-int _nodesCount;		//Total number of nodes (sqrt(_pairs))
+int _nodes;				//Total number of nodes (sqrt(_pairs))
 int _pairs;				//Total number of pairs (matrix size)
 int _pRows;				//Number of processes row (same as cols)
-int _pPairs;
+int _subPairs;			//Number of pairs per submatrix
+int _subNodes;			//Number of nodes per submatrix
 int p;					//Total number of active processes
 int k = 0;				//Current iteration (one iteration per row/col of processor)
 
@@ -70,59 +71,82 @@ int main(int argc, char* argv[])
 	}
 
 	//Send submatrices to processes
-	_pPairs = _pairs / p;
-	int *subdistance = new int[_pPairs];
-	int *subsequence = new int[_pPairs];
-	MPI_Scatter(&distanceMatrix[0], _pPairs, MPI_INT, subdistance, _pPairs, MPI_INT, 0, _mpiCommActiveProcesses);
-	
+	_subPairs = _pairs / p;
+	_subNodes = sqrt(_subPairs);
+	int *subdistance = new int[_subPairs];
+	int *subsequence = new int[_subPairs];
+	MPI_Scatter(&distanceMatrix[0], _subPairs, MPI_INT, subdistance, _subPairs, MPI_INT, 0, _mpiCommActiveProcesses);
+
 	// COUT PROPERLY FORMATTED TO KEEP
 	//	cout << "rank " << mpiRank << " - distance: " << subdistance[i] << endl;
 
-	_nodesCount = sqrt(_pairs);
+	_nodes = sqrt(_pairs);
 	_pRows = sqrt(p);
 
-	////Process position in row (x) and col (y)
-	int prow = mpiRank / _pRows;
-	int pcol = mpiRank % _pRows;
-
-	//Broadcast in rows
-	vector<int> receivedRowDistance;
-	receivedRowDistance.assign(subdistance, subdistance + _pPairs);
-	BroadcastRow(&receivedRowDistance[0]);
-
-	//Broadcast in cols
-	vector<int> receivedColDistance;
-	receivedColDistance.assign(subdistance, subdistance + _pPairs);
-	BroadcastCol(&receivedColDistance[0]);
-
-	//Calculate shortest path
-	if (pcol != k)
+	//Calculate the submatrices
+	while (k < _pRows)
 	{
-		//Calculate for each subk what's shortest path for all values of subdistance
-		subdistance[subk] = min(subdistance[subk], receivedColDistance[subk] + receivedRowDistance[subk]);
+		////Process position in row (x) and col (y)
+		int prow = mpiRank / _pRows;
+		int pcol = mpiRank % _pRows;
 
-		//Update sequence if new path shorter that old path
+		//Broadcast in rows
+		vector<int> receivedRowDistance;
+		receivedRowDistance.assign(subdistance, subdistance + _subPairs);
+		BroadcastRow(&receivedRowDistance[0]);
 
+		//Broadcast in cols
+		vector<int> receivedColDistance;
+		receivedColDistance.assign(subdistance, subdistance + _subPairs);
+		BroadcastCol(&receivedColDistance[0]);
+
+		//Calculate shortest path
+		//TODO: rows and cols and subrow and subcols are all messed up for sure
+		if (pcol != k && prow != k)
+		{
+			int subNode = 0;
+			int subk = 0;
+			int currentSubNode = 0;
+			//Updating the full submatrix at each sub iteration.
+			while (subk < _subNodes)
+			{
+				//Calculating the distance to intermediate node
+				int col = subk;
+				while (col < _subPairs)
+				{
+					//calculating the distance from intermediate node to destination
+					int row = subk * _subNodes;
+					while (row < (_subNodes * (subk + 1)))
+					{
+						int subcol = subNode / _subNodes;
+						int subrow = subNode % _subNodes;
+						bool isSelf = (pcol * _subNodes + subcol) == (prow * _subNodes + subrow);
+
+						if (!isSelf)
+						{
+							int newPathDistance = receivedColDistance[col] + receivedRowDistance[row];	//TODO: I must have swapped them.
+
+							if (newPathDistance != subdistance[currentSubNode])
+							{
+								int intermediateNodeAddress = (col % _subNodes) + (pcol * _subNodes);	//Need absolute address of intermediate node
+								subdistance[currentSubNode] = newPathDistance;
+								subsequence[currentSubNode] = intermediateNodeAddress;
+							}
+						}
+						++row;
+						++currentSubNode;
+					}
+					col += _subNodes;
+				}
+				++subk;
+			}
+		}
+		++k;
+		MPI_Barrier(_mpiCommActiveProcesses);
 	}
 
+	//TODO: Gather all submatrices into one and reassemble it
 
-	//if (pcol == k)
-	//{
-	//	cout << "rank " << mpiRank << " - pcol " << pcol << endl;
-
-	//	//TODO: finish trying broadcasting!
-	//	MPI_Bcast(subdistance, _nodesCount / _pRows, MPI_INT, 0, MPI_COMM_WORLD);
-	//	//Send col 0 to columnGroup
-	//}
-
-	////Send data in rows
-	//if (prow == k)
-	//{
-	//	cout << "rank " << mpiRank << " - prow " << prow << endl;
-	//	//Send row 0 to rowGroup
-	//}
-
-	MPI_Barrier(_mpiCommActiveProcesses);
 	if (mpiRank == 0)
 	{
 		//Stop chrono and print results
@@ -148,7 +172,7 @@ void DivideMatrix(int * matrix, int matrixSize, int submatricesCount)
 	matrixTemp.assign(matrix, matrix + matrixSize);
 
 	//all matrices are square here therefore row and col have the same size
-	int subRowSize = sqrt(matrixSize / submatricesCount);		
+	int subRowSize = sqrt(matrixSize / submatricesCount);
 	int rowSize = sqrt(matrixSize);
 	int matricesPerRow = sqrt(submatricesCount);
 	int i = 0;
@@ -192,11 +216,11 @@ void BroadcastRow(int *receivedRowDistance)
 {
 	int i = 0;
 	int col = 0;
-	
+
 	while (i < _pRows)
 	{
 		int broadcaster = col + (k * _pRows);
-		MPI_Bcast(receivedRowDistance, _nodesCount / _pRows, MPI_INT, broadcaster, _mpiRowProc[i]);
+		MPI_Bcast(receivedRowDistance, _nodes / _pRows, MPI_INT, broadcaster, _mpiRowProc[i]);
 		++col;
 		++i;
 	}
@@ -211,7 +235,7 @@ void BroadcastCol(int *receivedColDistance)
 	while (j < _pRows)
 	{
 		int broadcaster = (j * _pRows) + k;
-		MPI_Bcast(receivedColDistance, _nodesCount / _pRows, MPI_INT, broadcaster, _mpiColProc[j]);
+		MPI_Bcast(receivedColDistance, _nodes / _pRows, MPI_INT, broadcaster, _mpiColProc[j]);
 		++row;
 		++j;
 	}
@@ -246,14 +270,14 @@ vector<int> LoadInitialDistances()
 	}
 
 	_pairs = numbers.size();
-	_nodesCount = sqrt(_pairs);
+	_nodes = sqrt(_pairs);
 
 	int i = 0;
 	while (i < _pairs)
 	{
 		//Replace all distances to self with 0 instead of -1
 		numbers[i] = 0;
-		i += (_nodesCount +1);
+		i += (_nodes + 1);
 	}
 
 	return numbers;
@@ -269,7 +293,7 @@ vector<int> GetInitialSequences(vector<int> &initialDistance)
 		//Initial sequence filled with arrival node (direct link). If no connection between two nodes, then sequence set to -1
 		int toPush = initialDistance[i] == -1
 			? -1
-			: i % _nodesCount;				//arrival node id
+			: i % _nodes;				//arrival node id
 		initialSequence.push_back(toPush);
 		++i;
 	}
@@ -284,14 +308,14 @@ void MpiGroupInit()
 	//TO VERIFY!!
 	p = mpiSize > _pairs
 		? _pairs
-		: pow((int)((int)sqrt(mpiSize) - (_nodesCount % (int)sqrt(mpiSize))), 2);
+		: pow((int)((int)sqrt(mpiSize) - (_nodes % (int)sqrt(mpiSize))), 2);
 
 
 	// Remove all unnecessary ranks
 	if (mpiSize - p > 0)
 	{
 		MPI_Group newGroup;
-		
+
 		vector<int> toExclude;
 		int i = p;
 		while (i < mpiSize)
@@ -300,7 +324,7 @@ void MpiGroupInit()
 			++i;
 		}
 
-		MPI_Group_excl(world_group, mpiSize-p, &toExclude[0], &newGroup);
+		MPI_Group_excl(world_group, mpiSize - p, &toExclude[0], &newGroup);
 
 		// Create a new communicator
 		MPI_Comm_create(MPI_COMM_WORLD, newGroup, &_mpiCommActiveProcesses);
